62b8063640b9406f3acaa1018c441de0177b6d3e
ismailm0hammad <ismailmohammad7867+github@gmail.com>
2024-10-24 19:21:19 +0530

    Day 146

---
+++ Flip Equivalent Binary Trees.py
@@ -0,0 +1,14 @@
+# LeetCode 951 : Flip Equivalent Binary Trees
+# https://leetcode.com/problems/flip-equivalent-binary-trees/description/
+class Solution:
+  def flipEquiv(self, root1: TreeNode | None, root2: TreeNode | None) -> bool:
+    if not root1:
+      return not root2
+    if not root2:
+      return not root1
+    if root1.val != root2.val:
+      return False
+    return (self.flipEquiv(root1.left, root2.left) and
+            self.flipEquiv(root1.right, root2.right) or
+            self.flipEquiv(root1.left, root2.right) and
+            self.flipEquiv(root1.right, root2.left))
